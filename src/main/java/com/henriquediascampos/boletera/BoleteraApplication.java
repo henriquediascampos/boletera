4e93c83f00f8e7c852ecdcb661f4d38d2d646783
henrique.campos <henrique.campos@mv.com.br>
2023-12-10 13:08:52 -0300

    first commit

---
+++ src/main/java/com/henriquediascampos/boletera/BoleteraApplication.java
@@ -0,0 +1,13 @@
+package com.henriquediascampos.boletera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoleteraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoleteraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henriquediascampos/boletera/BoleteraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquediascampos.boletera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoleteraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
